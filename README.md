dd8ea2fed0f3c0e7b769018f3eb8d62e9cfb160f
Oxmel <oxm3l@hotmail.com>
2020-07-08 23:46:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# T2C Harvester - v0.1
+
+Ce script récupère et enregistre dans une base de donnée le nom ainsi que l'identifiant technique de chaque ligne et les directions/arrêts associés du réseau de transport T2C. Il est ensuite possible d'utiliser ces identifiants pour obtenir les horaires de passage en temps réel à un arrêt donné à l'aide du service [qr.t2c.fr](qr.t2c.fr).
+
+## Motivation
+
+A l'origine, ce script me servait à générer automatiquement une base de donnée pour l'application web [Bus'O'Matic](https://github.com/Oxmel/busomatic). Appli web que j'avais créé à l'époque pour un usage perso car les solutions déjà existantes ne me convenaient pas. 
+
+Aujourd'hui la situation est assez différente, et l'offre est pléthorique en matière de services et d'applications. Avec notamment les appli android [Auvergne Mobilité](https://play.google.com/store/apps/details?id=fr.auvergne.mobilite.android), mais également l'arrivée plus récente de [MyBus](https://play.google.com/store/apps/details?id=fr.monkeyfactory.mybusclermontferrand&hl=fr) ou encore l'appli web communautaire [t2c.app](https://twitter.com/ToshCamille/status/1244221407921389568). 
+
+En revanche, et bien que les données de navigation du réseau T2C soient disponibles sur l'api [navitia.io](navitia.io) depuis des années déjà, elles ne sont toujours pas accessibles publiquement. Et, comme la T2C ne semble pas particulièrement pressée de prendre le train de l'OpenData, ce script permet au moins d'offrir une solution alternative. En attendant qu'ils se réveillent et finissent, un jour peut-être, par ouvrir les données au grand public (#IWantToBelieve).
+
+## Exemples d'utilisation
+
+#### 1. Générer la base de donnée
+Pour générer une base de donnée,  clonez ce repo, installez les dépendances, et lancez le script :
+
+    $ git clone https://github.com/Oxmel/t2c-harvester
+    $ pip install BeautifulSoup4
+    $ python t2c-harvester.py
+
+#### 2. Accéder aux données
+
+Une fois la base de données générée, vous pouvez utiliser le language de votre choix pour vous connecter à la bdd et lancer des requêtes afin de récupérer les informations nécessaires.
+
+Voici un exemple basique en python pour afficher l'ensemble des lignes disponibles :
+
+    import sqlite3
+
+    database = ('t2c-database.sq3')
+    conn = sqlite3.connect(database)
+    lines = conn.execute("""SELECT id_ligne, nom, numero FROM lignes""")
+
+    for line in lines:
+        print line
+
+La référence `id_ligne` permet de récupérer les directions associées à une ligne spécifique :
+
+    SELECT id_direction, nom, numero FROM lignes WHERE id_ligne=1
+
+La référence `id_direction` permet de récupérer les arrêts correspondants à une direction :
+    
+    SELECT nom, numero FROM arrets WHERE id_direction=1
+
+#### 3. Récupérer les horaires de passage en temps réel
+
+Une fois que l'on connaît l'identifiant de l'arrêt basé sur la ligne et la direction, il suffit de passer cet identifiant en paramètre sur l'url qr.t2c.fr pour récupérer les horaires de passage des 10 prochains bus/tram à cet arrêt.
+
+Voici un autre exemple en python permettant d'extraire les valeurs avec `BeautifulSoup4` :
+
+
+    from bs4 import BeautifulSoup
+    import urllib
+    
+    # Ligne A - Direction La Pardieu Gare - Arrêt Jaude
+    stop_id = '3377704015495730'
+
+    url = 'http://qr.t2c.fr/qrcode?_stop_id='
+    conn = urllib.urlopen(url + stop_id)
+    soup = BeautifulSoup(conn, from_encoding='utf-8')
+    line_info = soup.find_all('td')[:3]
+
+    print 'Ligne: ' + line_info[0].text.strip()
+    print 'Direction: ' + line_info[1].text.strip()
+    print 'Prochain passage: ' + line_info[2].text.strip()
+
+A noter qu'ici, et pour simplifier l'exemple, ce snippet récupère seulement le 1er résultat. Mais si on veut récupérer l'ensemble des horaires, il suffit dans ce cas de créer une boucle et de stocker chaque résultat dans une liste.
+
---
+++ t2c-harvester.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+'''
+                            T2C Harvester v0.1
+
+Dans le cadre du fonctionnement de ses services de transport, la T2C a mis en
+place un système de numérotation technique (ou ID) de chacune de ses lignes.
+Chaque ligne dispose donc d'un nom (ex. 'Ligne B'), ainsi que d'un identifiant
+technique (ex. '11821953316814897'). Même chose pour chaque direction ainsi que
+chaque arrêt qui disposent eux aussi du même type d'identifiant.
+
+Ce numéro permet d'obtenir les horaires de passage en temps réel grâce à la
+page web 'qr.tc2.fr' qui est un service d'information par qrcode disponible à
+chaque arrêt. Cependant, en temps normal pour pouvoir utiliser ce service, il
+faut être physiquement présent à l'arrêt et flasher un qrcode avec son mobile
+afin de pouvoir accéder aux horaires en question.
+
+Ce script a pour but de récupérer les identifiants de chaque ligne régulière du
+réseau T2C et chaque direction / arrêt associé et de les stocker dans un base
+de donnée au format sqlite. Il est ensuite possible, à partir de cette bdd, de
+construire un système de recherche et d'affichage des horaires en temps réel
+pour n'importe quel arrêt. Pour un exemple d'application pratique, se référer
+au projet 'Bus-O-Matic' (https://github.com/Oxmel/busomatic).
+
+'''
+
+
+import urllib
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+import sqlite3
+import os
+import time
+import re
+
+
+# Url base permettant de récupérer les numéros de lignes
+line_url = 'http://auvergne-mobilite.fr/fr/schedule/line/?&lineSchedule[network]=network%3AT2C'
+# Url base des directions
+dir_url = 'http://www.t2c.fr/admin/synthese?SERVICE=page&p=17732927961956390&noline='
+# Url base des arrêts
+stop_url = 'http://www.t2c.fr/admin/synthese?SERVICE=page&p=17732927961956392&numeroroute='
+# Enregistre la base de donnée dans le même dossier que ce script
+db = os.path.abspath('t2c-database.sq3')
+conn = sqlite3.connect(db)
+# Curseur temporaire pour forger les requêtes SQL
+# Utiliser 'commit()' pour écrire définitivement dans la bdd
+cur = conn.cursor()
+
+
+# Récupère le nom / numéro de chaque ligne du réseau
+def get_lines():
+    line_list = OrderedDict()
+    req = urllib.urlopen(line_url)
+    soup = BeautifulSoup(req, from_encoding='utf-8', features='html.parser')
+    lines = soup.find('select', {'id': 'lineSchedule_line'}).find_all('option')[1:]
+
+    for line in lines:
+        try:
+            line_name = re.search(r'(^[A-C]|[0-9]{1,2})', line.text).group(1)
+            line_num = re.search(r'line:T2C:(.*)', line['value']).group(1)
+        # Ignore les lignes 'spéciales' qui ne sont pas exploitables
+        except AttributeError:
+            pass
+
+        line_list[line_name] = line_num
+
+    return line_list
+
+
+# Récupère chaque direction / arrêt pour une ligne donnée
+def get_line_data(url):
+    item_list = OrderedDict()
+    req = urllib.urlopen(url)
+    soup = BeautifulSoup(req, from_encoding='utf-8', features='html.parser')
+
+    # Skip le 1er résultat qui est un placeholder
+    for item in soup.find_all('option')[1:]:
+        item_name = item.text.strip()
+        item_num = item['value']
+        item_list[item_name] = item_num
+
+    return item_list
+
+
+# Créé la bdd et les tables qui seront utilisées pour stocker les data
+# On utilise un système de 'foreign keys' pour lier les tables entre elles
+# Ce système permet de rechercher facilement toutes les directions / arrêts
+# pour une ligne donnée (voir le README pour des exemples)
+def create_db():
+    print ('Création de la base de donnée...')
+    # Get foreign keys state (0 or 1)
+    # Récupère l'état de l'option foreign jeys (0 ou 1)
+    foreign_key = conn.execute('PRAGMA foreign_keys')
+    # La requête retourne un tuple donc on extrait la valeur
+    foreign_state = foreign_key.fetchone()[0]
+    if foreign_state == 0:
+        cur.execute('PRAGMA foreign_keys=ON')
+
+    # Table parent 'lignes'
+    cur.execute('''CREATE TABLE lignes(
+        id_ligne INTEGER PRIMARY KEY,
+        nom VARCHAR(10),
+        numero INTEGER)''')
+    # Table enfant de 'lignes'
+    cur.execute('''CREATE TABLE directions(
+        id_ligne INTEGER NOT NULL,
+        id_direction INTEGER PRIMARY KEY,
+        nom VARCHAR(10),
+        numero INTEGER,
+        FOREIGN KEY (id_ligne) REFERENCES lignes(id_ligne))''')
+    # Table enfant de 'directions' et petit enfant de 'lignes'
+    cur.execute('''CREATE TABLE arrets(
+        id_ligne INTEGER NOT NULL,
+        id_direction INTEGER NOT NULL,
+        id_arret INTEGER PRIMARY KEY,
+        nom VARCHAR(10),
+        numero INTEGER,
+        FOREIGN KEY (id_ligne) REFERENCES lignes(id_ligne),
+        FOREIGN KEY (id_direction) REFERENCES directions(id_direction))''')
+
+    conn.commit()
+
+
+# Scrap les infos des lignes / directions / arrêts et les stocke dans la bdd
+def fill_db():
+    line_list = get_lines()
+    for line_name, line_num in line_list.items():
+        print ('Traitement de la ligne %s' %line_name)
+        cur.execute('''INSERT INTO lignes(
+            nom, numero) VALUES(?, ?)''',
+            (line_name, line_num))
+
+        # ID sqlite de la ligne pour la relation parent/enfant
+        line_row_id = cur.execute('SELECT last_insert_rowid()')
+        line_key = line_row_id.fetchone()[0]
+        # ID de ligne en paramètre pour récupérer chaque direction
+        line_dir = get_line_data(dir_url + line_num)
+        for dir_name, dir_num in line_dir.items():
+            cur.execute('''INSERT INTO directions(
+                id_ligne, nom, numero) VALUES(?, ?, ?)''',
+                (line_key, dir_name, dir_num))
+
+            # ID sqlite de la direction
+            dir_row_id = cur.execute('SELECT last_insert_rowid()')
+            dir_key = dir_row_id.fetchone()[0]
+            # ID de direction en paramètre pour récupérer chaque arrêt
+            line_stop = get_line_data(stop_url + dir_num)
+            for stop_name, stop_num in line_stop.items():
+                cur.execute('''INSERT INTO arrets(
+                    id_ligne, id_direction, nom, numero) VALUES(?, ?, ?, ?)''',
+                    (line_key, dir_key, stop_name, stop_num))
+
+        # Pause entre chaque ligne pour éviter le spam de requêtes
+        time.sleep(3)
+
+    # Ecrit les infos dans la bdd
+    conn.commit()
+    cur.close()
+
+
+if __name__ == '__main__':
+    create_db()
+    fill_db()
+
